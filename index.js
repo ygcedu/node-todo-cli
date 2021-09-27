1d1c69c0a371f435296356e1e338122e9192e62f
ygcedu <937601471@qq.com>
2021-09-28 01:01:12 +0800

    showAll 功能扩展

--- index.js
+++ index.js
@@ -39,7 +39,61 @@ module.exports.showAll = async () => {
         value: '-2'
       }]
   }).then(answer => {
-    console.log(answer.index);
+    const index = parseInt(answer.index);
+    if (index >= 0) {
+      // 选中了一个任务
+      inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: '请选择操作',
+        choices: [
+          {name: '退出', value: 'quit'},
+          {name: '已完成', value: 'markAsDone'},
+          {name: '未完成', value: 'markAsUndone'},
+          {name: '改标题', value: 'updateTitle'},
+          {name: '删除', value: 'remove'}
+        ]
+      }).then(answer2 => {
+        switch (answer2.action) {
+          case 'markAsDone':
+            list[index].done = true;
+            db.write(list);
+            break;
+          case 'markAsUndone':
+            list[index].done = false;
+            db.write(list);
+            break;
+          case 'updateTitle':
+            inquirer.prompt({
+              type: 'input',
+              name: 'title',
+              message: '新的标题',
+              defaults: list[index].title
+            }).then(answer => {
+              list[index].title = answer.title;
+              db.write(list);
+            });
+            break;
+          case 'remove':
+            list.splice(index, 1);
+            db.write(list);
+            break;
+        }
+      });
+    } else if (index === -2) {
+      // 创建任务
+      inquirer.prompt({
+        type: 'input',
+        name: 'title',
+        message: '输入任务标题'
+      }).then(answer => {
+        list.push({
+          title: answer.title,
+          done: false
+        });
+        db.write(list);
+      });
+    }
   });
 };
 
