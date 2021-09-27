5a0f553dff992dfa65445d1542454e5895eb8ed2
ygcedu <937601471@qq.com>
2021-09-27 21:48:00 +0800

    commander.js 添加子命令

---
+++ index.js
@@ -0,0 +1,16 @@
+const program = require('commander');
+
+program
+  .option('-x, --xxx', 'what the x');
+
+program
+  .command('add')
+  .description('add a task')
+  .action((...args) => {
+    const words = args.slice(0, -1).join(' ');
+    console.log(words);
+  });
+
+program.parse(process.argv);
+
+console.log(program.xxx);
