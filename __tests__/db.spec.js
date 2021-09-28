e7f065cc4cf170075ab2fc6ce286932e02025b3d
ygcedu <937601471@qq.com>
2021-09-28 23:09:33 +0800

    使用真实文件系统白盒测试

--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -3,6 +3,8 @@ const db = require('../db.js');
 describe('db', () => {
   it('can read', () => {
     expect(db.read instanceof Function).toBe(true);
+    fs.writeFileSync('./xxx.json', `[{"title":"hi","done":true}]`);
+    db.read('/xxx.json');
   });
   it('can write', () => {
     expect(db.write instanceof Function).toBeTruthy();
