5058004d7ede914afad7015428f68628e2847645
ygcedu <937601471@qq.com>
2021-09-28 23:42:04 +0800

    完善文件读功能测试代码

--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -5,9 +5,10 @@ jest.mock('fs');
 
 describe('db', () => {
   it('can read', async () => {
-    fs.setMock('/xxx', null, '[]');
-    const list = await db.read('./xxx');
+    const data = [{title: 'hi', done: true}];
+    fs.setMock('/xxx', null, JSON.stringify(data));
+    const list = await db.read('/xxx');
     // 对比对象属性值都相等时使用 toStrictEqual
-    expect(list).toStrictEqual([]);
+    expect(list).toStrictEqual(data);
   });
 });
